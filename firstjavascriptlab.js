3a4942416663180aa64dbacce7c641da9b18fd85
djtoler <84260551+djtoler@users.noreply.github.com>
2021-08-09 09:07:33 -0400

    Create firstjavascriptlab.js

---
+++ firstjavascriptlab.js
@@ -0,0 +1,44 @@
+let Name = "Mitch Cuckovich";
+let age = 25;
+let birthday = "January 24";
+let pineapplePizza = true;
+let lifeEvents = [ "I was born in Troy, Michigan.", 
+                   "I went to Hope College", 
+                   "I went to Junior Olympics when I was 10 years old. I placed 14th in the nation in the 800 meter event.",
+                   "I'm a graduate of a Grand Circus bootcamp."
+ ]
+ let text = "";
+
+ if (pineapplePizza == true) {
+     console.log(`My name is ${Name} and I like pineapples on pizza. I am currently ${age} years old and my birthday is on ${birthday}.`);
+ }else{
+     console.log("My name is" +  Name  + "and I'm not into pineapples on pizza. I am currently " +  age + " years old and my birthday is on " +  birthday)
+ }
+
+
+ 
+
+ for (let i = 0; i < lifeEvents.length; i++){
+
+       console.log(lifeEvents[i])
+ }
+
+
+let counter = 0
+
+while(counter < 10){
+
+    counter++;
+    console.log (" The number is " + counter)
+}
+
+
+let randomNumber = Math.floor(Math.random() * 11)
+
+if(randomNumber !== 5){
+    counter++;
+    console.log(randomNumber + " != 5")
+}else{
+    counter++;
+    console.log(randomNumber + " === 5, ")
+
